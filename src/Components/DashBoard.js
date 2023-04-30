ff9089aaca8e0c64213357f6c51e616940c5cb09
Hema Moorthy <hemamoorthy8@gmail.com>
2023-04-30 18:40:38 +0530

    dashboard

--- src/Components/DashBoard.js
+++ src/Components/DashBoard.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import axios  from 'axios';
+import React, { useEffect ,useState} from 'react'
+
+
+
 
 const DashBoard = () => {
+    const [product,setProducts] = useState([]);
+
+    useEffect(() => {
+        togetAllProducts();
+    }, [])
+
+const togetAllProducts = async()=>{
+    let res = await axios.get("http://onlinetestapi.gerasim.in/api/Ecomm/GetAllProducts");
+    console.log(res);
+    setProducts(res.data.data);
+}
   return (
     <div>DashBoard</div>
   )
