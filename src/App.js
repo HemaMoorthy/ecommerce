3b224d49da2f149604796261ec0f0a7727b0b7f9
Hema Moorthy <hemamoorthy8@gmail.com>
2023-04-30 14:56:26 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from './Components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
     </div>
   );
 }
