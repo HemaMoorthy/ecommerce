ebaa80d899e8a4ce6656d5ad41bab1e9d27826d5
Hema Moorthy <hemamoorthy8@gmail.com>
2023-05-01 11:51:49 +0530

    cart

--- src/Components/DashBoard.js
+++ src/Components/DashBoard.js
@@ -7,6 +7,7 @@ import ProductDisplay from './ProductDisplay';
 
 const DashBoard = () => {
     const [product,setProducts] = useState([]);
+    const [cartCount,setCartcount] = useState(0);
 
     useEffect(() => {
         togetAllProducts();
@@ -16,16 +17,31 @@ const togetAllProducts = async()=>{
     let res = await axios.get("http://onlinetestapi.gerasim.in/api/Ecomm/GetAllProducts");
     console.log(res);
     setProducts(res.data.data);
+    showTotalCart();
+}
+
+
+const showTotalCart = async()=>{
+    let totalcartitems = await axios.get("http://onlinetestapi.gerasim.in/api/Ecomm/GetCartProductsByCustomerId");
+    console.log(totalcartitems.data.data.length);
+    setCartcount(totalcartitems.data.data.length)
 }
   return (
     <div className='container'>
         <div className='row'>
+            <div className='col-2 p-3'>
+            <button type="button" class="btn btn-primary">
+                Total Cart Item <span class="badge text-bg-secondary">{cartCount}</span>
+              </button>
+            </div>
+           
+              
         {
             product.map((item) => {
                 return(
                     <div className='col-4'>
 
-                        <ProductDisplay data={item}></ProductDisplay>
+                        <ProductDisplay data={item} callParent={()=>showTotalCart()}></ProductDisplay>
                     </div>
                 )
             })
--- src/Components/ProductDisplay.js
+++ src/Components/ProductDisplay.js
@@ -1,15 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Card.css";
+import axios from 'axios';
 
 const ProductDisplay = (props) => {
+
+    const [quantity,setQuantity] = useState(0);
+
+    const addQuantity = ()=>{
+        setQuantity(quantity+1)
+    } 
+
+    const subQuantity = ()=>{
+        setQuantity(quantity-1)
+    }
+
+    const addToCart= async()=>{
+        let obj={
+                "CartId": 0,
+                "CustId": 2,
+                "ProductId": props.data.productId,
+                "Quantity": quantity,
+                "AddedDate": new Date()     
+        }
+
+        let res = await axios.post("http://onlinetestapi.gerasim.in/api/Ecomm/AddToCart",obj);
+        console.log(res.data);
+        if(res.status===200){
+            alert("Successfully added to cart");
+            props.callParent();
+        }
+    }
+
+    
+    
+
     return (
-        <div className='container'>
-            <div className="card" style={{"width": "18rem;"}}>
+
+        <div className='card-container'>
+            <br /><br />
+            <div className="card" style={{ "width": "18rem;" }}>
                 <img src={props.data.productImageUrl} className="card-img-top" alt="..."></img>
-                    <div className="card-body">
-                        <h5 className="card-title">{ props.data.productName}</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                <div className="card-body">
+                    <h5 className="card-title">{props.data.productName}</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div class="btn-group me-2" role="group" aria-label="Second group">
+                        <button type="button" className="btn btn-secondary" onClick={()=>addQuantity()}>+</button>
+                        <button type="button" className="btn btn-secondary active">{quantity}</button>
+                        <button type="button" className="btn btn-secondary" onClick={()=>subQuantity()}>-</button>
                     </div>
+                    <button className="btn btn-primary" onClick={()=>addToCart()}>Add to Cart</button>
+                </div>
             </div>
         </div>
     )
